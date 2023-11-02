164907dc0e3d2fa45fa5e7fef3a278828a5fc23d
ahmaddzidnii <zdngaming312@gmail.com>
2023-11-02 16:00:42 +0700

    Tipe data string

---
+++ fundamental-golang/string.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Ahmad")
+	fmt.Println("Ahmad Zidni")
+	fmt.Println("Ahmad Zidni Hidayat")
+
+	// function untuk string
+
+	fmt.Println(len("Ahmad"))
+
+	// byte dari h
+	fmt.Println("Ahmad Zidni Hidayat"[1])
+
+}
