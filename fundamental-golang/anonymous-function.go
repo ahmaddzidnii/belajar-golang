65471c9fb50ececf03ce00d879254eff95d10ffb
ahmaddzidnii <zdngaming312@gmail.com>
2023-11-10 18:05:05 +0700

    anonymous function pada golang

---
+++ fundamental-golang/anonymous-function.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+type Blacklist func(string) bool
+
+func registerUser(name string, blacklist Blacklist) {
+	if blacklist(name) {
+		fmt.Println("You are blocked", name)
+	} else {
+		fmt.Println("Welcome", name)
+	}
+}
+
+func main() {
+	blacklist := func(name string) bool { //anyonymous function
+		return name == "anjing"
+	}
+	registerUser("anjing", blacklist)
+}
